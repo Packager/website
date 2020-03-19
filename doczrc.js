855317f72a9b36d10a909edf2f40ac315a5d08a8
Baryla <adrianrbarylski@gmail.com>
2020-03-19 20:39:38 +0000

    updated docs to reflect new changes

--- doczrc.js
+++ doczrc.js
@@ -1,18 +1,18 @@
 export default {
-    themesDir: "theme",
-    typescript: true,
-    title: "Packager Documentation",
-    ignore: ["readme.md"],
-    themeConfig: {
-        showDarkModeSwitch: false,
-        showLivePreview: false
-    },
-    repository: "https://github.com/baryla/packager",
-    menu: [
-        "Getting Started",
-        "Configuration",
-        "API Reference",
-        "Plugins",
-        "Roadmap"
-    ]
+  themesDir: "theme",
+  typescript: true,
+  title: "Packager Documentation",
+  ignore: ["readme.md"],
+  themeConfig: {
+    showDarkModeSwitch: false,
+    showLivePreview: false
+  },
+  repository: "https://github.com/baryla/packager",
+  menu: [
+    "Getting Started",
+    "Configuration",
+    "API Reference",
+    "Plugins",
+    "Roadmap"
+  ]
 };
