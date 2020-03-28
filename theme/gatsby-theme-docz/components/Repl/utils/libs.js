dad81ab013c432a74f0b9aa59bd2e184e2bab0a1
Baryla <adrianrbarylski@gmail.com>
2020-03-28 02:16:10 +0000

    updated lib paths

--- theme/gatsby-theme-docz/components/Repl/utils/libs.js
+++ theme/gatsby-theme-docz/components/Repl/utils/libs.js
@@ -1,15 +1,17 @@
 const ROOT = "https://cdn.jsdelivr.net/npm";
 
+const cacheBusting = Date.now();
+
 export default {
-  packager: `${ROOT}/packager@latest`,
-  coffeescript: `${ROOT}/packager-plugin-coffeescript@latest`,
-  commonjs: `${ROOT}/packager-plugin-commonjs@latest`,
-  css: `${ROOT}/packager-plugin-css@latest`,
-  json: `${ROOT}/packager-plugin-json@latest`,
-  less: `${ROOT}/packager-plugin-less@latest`,
-  sass: `${ROOT}/packager-plugin-sass@latest`,
-  stylus: `${ROOT}/packager-plugin-stylus@latest`,
-  svelte: `${ROOT}/packager-plugin-svelte@latest`,
-  ts: `${ROOT}/packager-plugin-typescript@latest`,
-  vue: `${ROOT}/packager-plugin-vue@latest`
+  packager: `${ROOT}/packager@latest/dist/index.browser.js?${cacheBusting}`,
+  coffeescript: `${ROOT}/packager-plugin-coffeescript@latest/dist/index.browser.js?${cacheBusting}`,
+  commonjs: `${ROOT}/packager-plugin-commonjs@latest/dist/index.browser.js?${cacheBusting}`,
+  css: `${ROOT}/packager-plugin-css@latest/dist/index.browser.js?${cacheBusting}`,
+  json: `${ROOT}/packager-plugin-json@latest/dist/index.browser.js?${cacheBusting}`,
+  less: `${ROOT}/packager-plugin-less@latest/dist/index.browser.js?${cacheBusting}`,
+  sass: `${ROOT}/packager-plugin-sass@latest/dist/index.browser.js?${cacheBusting}`,
+  stylus: `${ROOT}/packager-plugin-stylus@latest/dist/index.browser.js?${cacheBusting}`,
+  svelte: `${ROOT}/packager-plugin-svelte@latest/dist/index.browser.js?${cacheBusting}`,
+  ts: `${ROOT}/packager-plugin-typescript@latest/dist/index.browser.js?${cacheBusting}`,
+  vue: `${ROOT}/packager-plugin-vue@latest/dist/index.browser.js?${cacheBusting}`
 };
